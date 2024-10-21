0fb0fd6eca2f6ab362e1894a38d526fa7d3868ae
Tommaso Pettinari <t0mmaso.p@hotmail.it>
2024-10-21 15:49:22 +0200

    sinking with liquid

--- data_tommaso/sinking_with_liquid/plot_displacement.py
+++ data_tommaso/sinking_with_liquid/plot_displacement.py
@@ -10,15 +10,14 @@ ax = plt.gca()
 time = []
 displacement = []
 
-#file_name = f'/home/tommaso/Projects/rheodata/creep_rising/2024-06-25.mqa'
-file_name = '/home/tommaso/Projects/rheodata/sinking_with_liquid/sinking_with_liquid_3bis.dat'
+file_name = '/home/tommaso/Projects/creep_rising_sinking/data_tommaso/sinking_with_liquid/sinking_with_liquid_2bis.dat'
 
 data = open(file_name, 'r') #, encoding="utf-16"
 lines = data.readlines()
 
 #lines.pop(0)
 
-for count in range(66):
+for count in range(85):
     obs = lines[count].split()
     #time.append((float(obs[1])))
     displacement.append((float(obs[0])))
@@ -27,21 +26,21 @@ y_offset = displacement[0]
 
 pix_to_cm = 4/733
 displacement = [(i - y_offset)*pix_to_cm for i in displacement]
-interval = 10
+interval = 5
 time = [n*interval for n in range(len(displacement))]
 
-ax.scatter(time, displacement, marker='.', linewidths=1, label=f'data')
+ax.scatter(time, displacement, marker='.', linewidths=1, label=f'dataset 2')
 
 #ax.set_xscale('log')
 #ax.set_yscale('log')
 
 #p = np.polyfit(time[9000:70000], displacement[9000:70000], 1)
 
-xlin1 = np.linspace(1, 800, 10^5)
-ylin1 = 0.015*xlin1 + 0.5
+xlin1 = np.linspace(1, 400, 10^5)
+ylin1 = 0.03*xlin1 + 0.5
 ylin2 = 0.35*np.sqrt(xlin1)
-plt.plot(xlin1, ylin2, c='red', label=f'y=sqrt(x)')
-plt.plot(xlin1, ylin1, c='black', linestyle='dashed', label=f'y= 0.015 x')
+#plt.plot(xlin1, ylin2, c='red', label=f'y=sqrt(x)')
+plt.plot(xlin1, ylin1, c='black', linestyle='dashed', label=f'y= 0.03 x')
 
 
 #x1 = np.linspace(0,10**3,10**6)
@@ -51,4 +50,4 @@ ax.set_xlabel('time (s)', fontsize = 15.0)
 ax.set_ylabel('displacement (cm)', fontsize = 15.0)
 ax.legend(loc='upper left', fontsize=12.0)            
 plt.show()
-fig.savefig(f'sinking_with_liquid/sinking_1bis.png', format='png', bbox_inches='tight')          
+fig.savefig(f'/home/tommaso/Projects/creep_rising_sinking/data_tommaso/sinking_with_liquid/sinking_2.png', format='png', bbox_inches='tight')          
